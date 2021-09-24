60b5b0b3a812e80c6b00e981e32b85744ade1770
Ryan Hewitt <ryan@mesuva.com.au>
2021-09-24 17:32:08 +0100

    V9 support

--- single_pages/dashboard/sitemap/add_multiple_pages.php
+++ single_pages/dashboard/sitemap/add_multiple_pages.php
@@ -12,31 +12,31 @@ $ci = $app->make('helper/concrete/ui');
                 <legend><?php echo t('Step 2 - Configure and create pages'); ?></legend>
             <?php } ?>
             <div class="form-group">
-                <label for="page_type"><?php echo t('Page Type'); ?></label>
+                <label for="page_type" class="form-label"><?php echo t('Page Type'); ?></label>
                 <p><em><?php echo $pageType->getPageTypeDisplayName(); ?></em>
 
-                <?php if(!$onepagetype) { ?>
-                    - <a href="<?=$view->url('/dashboard/sitemap/add_multiple_pages');?>"><?=t('change')?></a>
-                <?php } ?>
+                    <?php if(!$onepagetype) { ?>
+                        - <a href="<?=$view->url('/dashboard/sitemap/add_multiple_pages');?>"><?=t('change')?></a>
+                    <?php } ?>
                 </p>
             </div>
 
             <div class="form-group">
-                <label for="parent_page"><?php echo t('Parent Page'); ?></label>
+                <label for="parent_page" class="form-label"><?php echo t('Parent Page'); ?></label>
                 <?php $pageSelector = Loader::helper('form/page_selector');
-                echo $pageSelector->selectPage('parent_page', ($emptysite ? HOME_CID : null), 'ccm_selectSitemapNode');
+                echo $pageSelector->selectPage('parent_page', ($emptysite ? \Concrete\Core\Page::getHomePageID() : null));
                 ?>
             </div>
 
             <div class="form-group">
-                <label for="page_template"><?php echo t('Page Template'); ?></label>
+                <label for="page_template" class="form-label"><?php echo t('Page Template'); ?></label>
                 <?php echo $form->select('page_template', $pageTemplates) ?>
             </div>
 
             <div class="form-group">
-                <label for="pagenames"><?php echo t('Page Names (enter one per line)'); ?></label>
-                <?php echo $form->textarea('pagenames', '', array('rows' => 6, 'class' => 'span2', 'placeholder' => t('Page names, one per line'))) ?>
-                <span class="help-block"><?=t('To create nested pages, indent child pages using dashes.');?></span>
+                <label for="pagenames" class="form-label"><?php echo t('Page Names (enter one per line)'); ?></label>
+                <?php echo $form->textarea('pagenames', '', array('rows' => 6, 'class' => 'form-control', 'placeholder' => t('Page names, one per line'))) ?>
+                <div class="help-block"><?=t('To create nested pages, indent child pages using dashes.');?></div>
             </div>
         </fieldset>
 
@@ -48,19 +48,18 @@ $ci = $app->make('helper/concrete/ui');
         ?>
 
         <?php if (strpos($output, 'label') !== false) { ?>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title"><?php echo t('Page Defaults'); ?></h3>
-                <span class="help-block"
-                      style="margin-bottom: 0;"><?= t('Values entered below will be applied to all new pages'); ?></span>
-            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><?php echo t('Page Defaults'); ?></h3>
+                    <p><?= t('Values entered below will be applied to all new pages'); ?></p>
+                </div>
 
-            <div class="panel-body">
-                <div id="defaults" style="display: none">
-                    <?php echo $output; ?>
+                <div class="panel-body">
+                    <div id="defaults" style="display: none">
+                        <?php echo $output; ?>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php } ?>
 
         <div class="ccm-dashboard-form-actions-wrapper">
@@ -68,7 +67,7 @@ $ci = $app->make('helper/concrete/ui');
                 <?php if (!$onepagetype) { ?>
                     <?php echo $ci->button(t('Select Page Type'), $view->url('/dashboard/sitemap/add_multiple_pages'), 'left'); ?>
                 <?php } ?>
-                <button class="pull-right btn btn-success"
+                <button class="pull-right float-right float-end btn btn-primary"
                         type="submit"><?php echo t('Create Pages') ?></button>
             </div>
         </div>
@@ -83,18 +82,18 @@ $ci = $app->make('helper/concrete/ui');
         });
     </script>
 <?php } else { ?>
-<fieldset>
-    <legend><?php echo t('Step 1 - Select Page Type'); ?></legend>
-    <table class="table table-striped">
-        <?php foreach ($pageTypes as $key => $pagetype) { ?>
-            <tr>
-                <td>
-                    <a href="<?php echo $this->url('/dashboard/sitemap/add_multiple_pages', $key); ?>"><?php echo $pagetype->getPageTypeDisplayName(); ?></a>
-                </td>
-            </tr>
-        <?php } ?>
-    </table>
-</fieldset>
+    <fieldset>
+        <legend><?php echo t('Step 1 - Select Page Type'); ?></legend>
+        <table class="table table-striped">
+            <?php foreach ($pageTypes as $key => $pagetype) { ?>
+                <tr>
+                    <td>
+                        <a href="<?php echo $this->url('/dashboard/sitemap/add_multiple_pages', $key); ?>"><?php echo $pagetype->getPageTypeDisplayName(); ?></a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+    </fieldset>
 <?php } ?>
 
 <?php
